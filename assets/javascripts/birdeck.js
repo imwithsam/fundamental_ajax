c48de3f6b84e95f2e8e94e25abcb9e44b25a6469
Samson Brock <SamsonLB@gmail.com>
2015-09-21 10:18:14 -0600

    Add AJAX call to create a new post

--- assets/javascripts/birdeck.js
+++ assets/javascripts/birdeck.js
@@ -16,4 +16,29 @@ $(document).ready(function() {
       });
     }
   });
+
+  $('#create-post').on('click', function() {
+    var postParams = {
+      post: {
+        description: $('#post-description').val()
+      }
+    };
+
+    $.ajax({
+      type: 'POST',
+      url:  'https://turing-birdie.herokuapp.com/api/v1/posts.json',
+      data: postParams,
+      success: function(post) {
+          $('#latest-posts').append(
+            '<div class="post" data-id="'
+            + post.id
+            + '"><h6>Published on '
+            + post.created_at
+            + '</h6><p>'
+            + post.description
+            + '</p></div>'
+            );
+      }
+    });
+  });
 });
